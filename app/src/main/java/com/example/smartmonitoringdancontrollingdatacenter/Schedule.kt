827b845bfe13f69825b539c641f9c46bf8c679b6
Irvandi19 <irvandi.bibrik@gmail.com>
2024-07-10 05:34:42 +0700

    Delete app/src/main/java/com/example/smartmonitoringdancontrollingdatacenter directory

--- app/src/main/java/com/example/smartmonitoringdancontrollingdatacenter/Schedule.kt
+++
@@ -1,8 +0,0 @@
-package com.example.smartmonitoringdancontrollingdatacenter
-
-data class Schedule(
-    val startHour: Int = 0,
-    val startMinute: Int = 0,
-    val endHour: Int = 0,
-    val endMinute: Int = 0
-)
